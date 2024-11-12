ec16be8ec3c917952996cf92ff7e57739952db35
powermaker450 <contact@povario.com>
2024-11-12 16:09:20 -0500

    Add some settings pages

--- components/SettingsPage.tsx
+++ components/SettingsPage.tsx
@@ -1,8 +1,31 @@
-import { Appbar, List } from "react-native-paper";
+import { Appbar, List, useTheme } from "react-native-paper";
 import { router } from "expo-router";
 import { ScrollView } from "react-native";
+import { useEffect, useState } from "react";
+import { storage } from "@/util/storage";
+import Panel from "@/util/Panel";
 
 export default function SettingsPage() {
+  const theme = useTheme();
+  const [admin, setAdmin] = useState(false);
+
+  useEffect(() => {
+    const settings = JSON.parse(storage.getString("settings")!);
+    const panel = new Panel(settings);
+
+    panel.getScopes().then(scopes => {
+      for (const scope of scopes) {
+        switch (scope) {
+          case "servers.admin":
+            setAdmin(true);
+            break;
+          default:
+            {}
+        }
+      }
+    })
+  })
+
   return (
     <>
       <Appbar.Header>
@@ -13,6 +36,14 @@ export default function SettingsPage() {
         contentContainerStyle={{ marginLeft: 15, justifyContent: "center" }}
       >
         <List.Item
+          title="Panel Settings"
+          description="Manage your Pufferpanel instance"
+          style={{ display: admin ? "flex" : "none" }}
+          onPress={() => console.log("admin")}
+          left={() => <List.Icon icon="shield-edit" />}
+        />
+
+        <List.Item
           title="Account"
           description="Manage account settings"
           onPress={() => router.navigate("/settings/account")}
@@ -21,9 +52,10 @@ export default function SettingsPage() {
 
         <List.Item
           title="Appearance"
+          disabled
           description="Look and feel of the app"
           onPress={() => router.navigate("/settings/appearance")}
-          left={() => <List.Icon icon="palette" />}
+          left={() => <List.Icon icon="palette" color={theme.colors.onSurfaceDisabled} />}
         />
       </ScrollView>
     </>
