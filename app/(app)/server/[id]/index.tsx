0bf3116ac97ca9dbd4e54a9d1da4a6205923e77f
powermaker450 <contact@povario.com>
2024-11-19 16:14:59 -0500

    Fix whole page re-render when editing the server name

--- app/(app)/server/[id]/index.tsx
+++ app/(app)/server/[id]/index.tsx
@@ -17,42 +17,27 @@
  */
 
 import Panel from "@/util/Panel";
-import { router, useLocalSearchParams, useNavigation } from "expo-router";
+import { useLocalSearchParams, useNavigation } from "expo-router";
 import { useEffect, useState } from "react";
 import {
-  ActivityIndicator,
-  Appbar,
-  Icon,
-  Tooltip,
-  TextInput,
-  useTheme,
-  Dialog,
-  Portal,
-  Button,
   BottomNavigationRoute,
   BottomNavigation,
   Text
 } from "react-native-paper";
-import haptic, { handleTouch } from "@/util/haptic";
 import PufferpanelSocket from "@/util/PufferpanelSocket";
 import ConsoleView from "@/components/ConsoleView";
-import Notice from "@/components/Notice";
 import NavBar from "@/components/NavBar";
 import CustomView from "@/components/CustomView";
 import ServerManagePage from "@/components/ServerManagePage";
+import NameTab from "@/components/NameTab";
 
 export default function ServerScreen() {
-  const control = Panel.getPanel();
-  const theme = useTheme();
-
   const { id } = useLocalSearchParams();
+  const control = Panel.getPanel();
+
   const navigation = useNavigation();
-  const [serverName, setServerName] = useState("");
-  const [newName, setNewName] = useState("");
-  const [editServerPerms, setEditServerPerms] = useState(false);
-  const [startPerms, setStartPerms] = useState(false);
-  const [stopPerms, setStopPerms] = useState(false);
   const [sendConsolePerms, setSendConsolePerms] = useState(false);
+  const [running, setRunning] = useState(false);
 
   let serverSocket: PufferpanelSocket;
 
@@ -87,15 +72,9 @@ export default function ServerScreen() {
   });
 
   useEffect(() => {
-    control.get.server(id as string).then(({ server, permissions }) => {
+    control.get.server(id as string).then(({ permissions }) => {
       // First view stuff
-      setServerName(server.name);
-      setNewName(server.name);
-
-      setEditServerPerms(permissions.editServerData);
       setSendConsolePerms(permissions.sendServerConsole);
-      setStartPerms(permissions.startServer);
-      setStopPerms(permissions.stopServer);
 
       if (permissions.viewServerConsole) {
         serverSocket = control.getSocket(id as string);
@@ -143,176 +122,13 @@ export default function ServerScreen() {
     });
   }, []);
 
-  const [running, setRunning] = useState<boolean | undefined>(false);
-  const [loading, setLoading] = useState(false);
   const [logs, setLogs] = useState("");
-  const [nameUpdating, setNameUpdating] = useState(false);
-  const [notice, setNotice] = useState(false);
-  const [visible, setVisible] = useState(false);
-  const openNameChange = () => {
-    if (!editServerPerms) {
-      return;
-    }
-
-    haptic();
-    setVisible(true);
-  };
-  const closeNameChange = () => {
-    setVisible(false);
-    setNewName(serverName);
-  };
-
-  const handleStart = () => {
-    setLoading(true);
-    control.get
-      .server(id as string)
-      .then(({ server }) => server.actions.start())
-      .catch(err => console.warn("An unexpected error occured:", err))
-      .finally(() => {
-        setRunning(true);
-        setLoading(false);
-      });
-  };
-
-  const handleStop = () => {
-    setLoading(true);
-    control.get
-      .server(id as string)
-      .then(({ server }) => server.actions.stop())
-      .catch(err => console.warn("An unexpected error occured:", err))
-      .finally(() => {
-        setLoading(false);
-      });
-  };
-
-  const handleKill = () => {
-    setLoading(true);
-    control.get
-      .server(id as string)
-      .then(({ server }) => server.actions.kill())
-      .catch(err => console.warn("An unexpected error occured:", err))
-      .finally(() => {
-        setLoading(false);
-      });
-  };
-
-  const handleNameChange = () => {
-    setNameUpdating(true);
-    control.get
-      .server(id as string)
-      .then(({ server }) => {
-        server.edit.name(newName).then(() => {
-          setServerName(newName);
-          setNewName(newName);
-          haptic("notificationSuccess");
-          setNotice(true);
-          setTimeout(() => {
-            setNotice(false);
-          }, 2000);
-        });
-      })
-      .catch(err => console.warn("An unexpected error occured:", err))
-      .finally(() => {
-        setNameUpdating(false);
-        closeNameChange();
-      });
-  };
-
-  const loadingIcon = <ActivityIndicator animating={true} />;
-
-  const startButton = (
-    <Tooltip title="Start" enterTouchDelay={300} leaveTouchDelay={150}>
-      <Appbar.Action
-        icon="play-outline"
-        onPress={handleStart}
-        onPressIn={handleTouch}
-        disabled={!startPerms}
-      />
-    </Tooltip>
-  );
-
-  const stopButton = (
-    <Tooltip title="Stop" enterTouchDelay={300} leaveTouchDelay={150}>
-      <Appbar.Action
-        icon="stop"
-        onPress={handleStop}
-        onPressIn={handleTouch}
-        disabled={!stopPerms}
-      />
-    </Tooltip>
-  );
-
-  const killButton = (
-    <Tooltip title="Kill" enterTouchDelay={300} leaveTouchDelay={150}>
-      <Appbar.Action
-        icon="skull-outline"
-        onPress={handleKill}
-        onPressIn={handleTouch}
-        disabled={!stopPerms}
-      />
-    </Tooltip>
-  );
 
   return (
     <>
-      <Appbar.Header>
-        <Appbar.BackAction
-          onPress={() => router.back()}
-          onPressIn={handleTouch}
-        />
-        <Icon
-          source="circle"
-          size={12}
-          color={running ? theme.colors.primary : theme.colors.surfaceDisabled}
-        />
-        <Appbar.Content
-          style={{ marginLeft: 10 }}
-          title={serverName}
-          onPress={openNameChange}
-          disabled={!editServerPerms}
-        />
-
-        {loading ? loadingIcon : running ? stopButton : startButton}
-        {!loading && running && killButton}
-      </Appbar.Header>
-
-      <Portal>
-        <Dialog
-          visible={visible}
-          onDismiss={closeNameChange}
-          dismissable={!nameUpdating}
-        >
-          <Dialog.Title>Edit Name</Dialog.Title>
-          <Dialog.Content>
-            <TextInput
-              mode="outlined"
-              label="Server Name"
-              value={newName}
-              onChangeText={newText => setNewName(newText)}
-            />
-          </Dialog.Content>
-          <Dialog.Actions>
-            {!nameUpdating && <Button onPress={closeNameChange}>Cancel</Button>}
-            {nameUpdating ? (
-              loadingIcon
-            ) : (
-              <Button
-                onPressIn={handleTouch}
-                onPress={handleNameChange}
-                disabled={newName === serverName || !newName.trim()}
-                mode="contained"
-                style={{ paddingLeft: 10, paddingRight: 10 }}
-              >
-                Save
-              </Button>
-            )}
-          </Dialog.Actions>
-        </Dialog>
-      </Portal>
+      <NameTab running={running} />  
 
       <NavBar routes={routes} renderScene={renderScene} />
-
-      <Notice condition={notice} setCondition={setNotice} text="Name saved!" />
     </>
   );
 }
---
+++ components/NameTab.tsx
@@ -0,0 +1,214 @@
+/*
+ * puffMob: A mobile client for Pufferpanel
+ * Copyright (C) 2024 powermaker450
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import Panel from '@/util/Panel';
+import haptic, { handleTouch } from '@/util/haptic';
+import { router, useLocalSearchParams } from 'expo-router';
+import React, { useEffect, useState } from 'react'
+import { ActivityIndicator, Appbar, Button, Dialog, Icon, Portal, TextInput, Tooltip, useTheme } from 'react-native-paper';
+
+interface NameTabProps {
+  running: boolean;
+}
+
+const NameTab = ({ running }: NameTabProps) => {
+  const { id } = useLocalSearchParams();
+  const theme = useTheme();
+  const control = Panel.getPanel();
+
+  const [serverName, setServerName] = useState("");
+  const [newName, setNewName] = useState("");
+  const [editServer, setEditServer] = useState(false);
+  const openNameChange = () => {
+    if (!editServer) {
+      return;
+    }
+
+    haptic();
+    setVisible(true);
+  };
+  const closeNameChange = () => {
+    setVisible(false);
+    setNewName(serverName);
+  };
+
+  const [startPerms, setStartPerms] = useState(false);
+  const [stopPerms, setStopPerms] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+  const [nameUpdating, setNameUpdating] = useState(false);
+  const [visible, setVisible] = useState(false);
+
+  const handleStart = () => {
+    setLoading(true);
+    control.get
+      .server(id as string)
+      .then(({ server }) => server.actions.start())
+      .catch(err => console.warn("An unexpected error occured:", err))
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const handleStop = () => {
+    setLoading(true);
+    control.get
+      .server(id as string)
+      .then(({ server }) => server.actions.stop())
+      .catch(err => console.warn("An unexpected error occured:", err))
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const handleKill = () => {
+    setLoading(true);
+    control.get
+      .server(id as string)
+      .then(({ server }) => server.actions.kill())
+      .catch(err => console.warn("An unexpected error occured:", err))
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const handleNameChange = () => {
+    setNameUpdating(true);
+    control.get
+      .server(id as string)
+      .then(({ server }) => {
+        server.edit.name(newName).then(() => {
+          setServerName(newName);
+          setNewName(newName);
+          haptic("notificationSuccess");
+        });
+      })
+      .catch(err => console.warn("An unexpected error occured:", err))
+      .finally(() => {
+        setNameUpdating(false);
+        closeNameChange();
+      });
+  };
+
+  const loadingIcon = <ActivityIndicator animating={true} />;
+
+  const startButton = (
+    <Tooltip title="Start" enterTouchDelay={300} leaveTouchDelay={150}>
+      <Appbar.Action
+        icon="play-outline"
+        onPress={handleStart}
+        onPressIn={handleTouch}
+        disabled={!startPerms}
+      />
+    </Tooltip>
+  );
+
+  const stopButton = (
+    <Tooltip title="Stop" enterTouchDelay={300} leaveTouchDelay={150}>
+      <Appbar.Action
+        icon="stop"
+        onPress={handleStop}
+        onPressIn={handleTouch}
+        disabled={!stopPerms}
+      />
+    </Tooltip>
+  );
+
+  const killButton = (
+    <Tooltip title="Kill" enterTouchDelay={300} leaveTouchDelay={150}>
+      <Appbar.Action
+        icon="skull-outline"
+        onPress={handleKill}
+        onPressIn={handleTouch}
+        disabled={!stopPerms}
+      />
+    </Tooltip>
+  );
+
+  useEffect(() => {
+    control.get.server(id as string).then(({ server, permissions }) => {
+      setServerName(server.name);
+      setNewName(server.name);
+
+      setEditServer(permissions.editServerData);
+      setStartPerms(permissions.startServer);
+      setStopPerms(permissions.stopServer);
+    })
+  }, []);
+
+  return (
+    <>
+      <Appbar.Header>
+        <Appbar.BackAction
+          onPress={() => router.back()}
+          onPressIn={handleTouch}
+        />
+        <Icon
+          source="circle"
+          size={12}
+          color={running ? theme.colors.primary : theme.colors.surfaceDisabled}
+        />
+        <Appbar.Content
+          style={{ marginLeft: 10 }}
+          title={serverName}
+          onPress={openNameChange}
+          disabled={!editServer}
+        />
+
+        {loading ? loadingIcon : running ? stopButton : startButton}
+        {!loading && running && killButton}
+      </Appbar.Header>
+
+      <Portal>
+        <Dialog
+          visible={visible}
+          onDismiss={closeNameChange}
+          dismissable={!nameUpdating}
+        >
+          <Dialog.Title>Edit Name</Dialog.Title>
+          <Dialog.Content>
+            <TextInput
+              mode="outlined"
+              label="Server Name"
+              value={newName}
+              onChangeText={newText => setNewName(newText)}
+            />
+          </Dialog.Content>
+          <Dialog.Actions>
+            {!nameUpdating && <Button onPress={closeNameChange}>Cancel</Button>}
+            {nameUpdating ? (
+              loadingIcon
+            ) : (
+              <Button
+                onPressIn={handleTouch}
+                onPress={handleNameChange}
+                disabled={newName === serverName || !newName.trim()}
+                mode="contained"
+                style={{ paddingLeft: 10, paddingRight: 10 }}
+              >
+                Save
+              </Button>
+            )}
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </>
+  );
+}
+
+export default NameTab;
