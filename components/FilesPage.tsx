5036af83f8d6282dae1a97453baff65652e7a998
powermaker450 <contact@povario.com>
2024-11-26 12:28:39 -0500

    Fix FilesPage

--- components/FilesPage.tsx
+++ components/FilesPage.tsx
@@ -1,3 +1,21 @@
+/*
+ * puffMob: A mobile client for Pufferpanel
+ * Copyright (C) 2024 powermaker450
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { ActivityIndicator, Button, List, Text, TextInput } from "react-native-paper";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import { useEffect, useState } from "react";
@@ -25,7 +43,7 @@ const FilesPage = () => {
 
   const loadingText = <ActivityIndicator animating />;
   const errorText = (
-    <View style={{ maxWidth: "85%", margin: "auto" }}>   
+    <View style={{ maxWidth: "85%", margin: "auto", marginTop: 30 }}>   
       <Text variant="displaySmall" style={{ marginBottom: 15 }}>An error occured.</Text>
 
       <Text
